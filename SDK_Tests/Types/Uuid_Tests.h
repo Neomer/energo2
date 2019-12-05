9fb1b3262414aabf10236c7f47d364cd7b6cfb37
Vinokurov Kirill <vinokurov@elewise.com>
2019-12-05 14:52:36 +0400

    tests

---
+++ SDK_Tests/Types/Uuid_Tests.h
@@ -0,0 +1,18 @@
+//
+// Created by vinokurov on 05.12.2019.
+//
+
+#ifndef ENERGO_UUID_TESTS_H
+#define ENERGO_UUID_TESTS_H
+
+#include <gtest/gtest.h>
+#include <Types/Uuid.h>
+
+namespace energo::types {
+
+class Uuid_Tests: public testing::Test {
+
+};
+
+}
+#endif //ENERGO_UUID_TESTS_H
